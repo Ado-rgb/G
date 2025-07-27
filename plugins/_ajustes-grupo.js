fb9fa4280e330678680d7c7b8acf232eac6354f4
Ado <minexdt@gmail.com>
2025-07-26 21:18:07 -0600

    Actualizar _ajustes-grupo.js

--- plugins/_ajustes-grupo.js
+++ plugins/_ajustes-grupo.js
@@ -1,7 +1,9 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!m.isGroup) return m.reply('❌ Este comando solo funciona en grupos')
+  if (!m.isGroup) return m.reply('✿ Este comando solo funciona en grupos')
 
-  if (!args[0]) return m.reply(
+  let chat = global.db.data.chats[m.chat]
+  if (!args[0]) {
+    return m.reply(
 `✿ *Uso correcto ›* ${usedPrefix + command} <función>
 
 Funciones disponibles:
@@ -14,37 +16,20 @@ Funciones disponibles:
 Ejemplo:
 > ${usedPrefix}on welcome
 > ${usedPrefix}off welcome`
-  )
+    )
+  }
 
-  let chat = global.db.data.chats[m.chat] = global.db.data.chats[m.chat] || {}
   let option = args[0].toLowerCase()
   let valid = ['antilink', 'detect', 'alerts', 'antiarabe', 'welcome']
 
-  if (!valid.includes(option)) return m.reply(
-`✿ *Función no válida ›* ${option}
-> Usa alguna de estas: ${valid.join(', ')}`
-  )
+  if (!valid.includes(option)) {
+    return m.reply(`✿ *Función no válida ›* ${option}
+> Usa alguna de estas: ${valid.join(', ')}`)
+  }
 
   if (command === 'on') {
     chat[option] = true
     m.reply(`✿ *${option}* activado ✨`)
-
-    if (option === 'welcome') {
-      await conn.sendMessage(m.chat, {
-        text: `✿ *Welcome activado* en este grupo\n> Bienvenidos y bienvenidas siempre.`,
-        contextInfo: {
-          externalAdReply: {
-            title: "Sistema de Bienvenida",
-            body: "Conectado y listo para saludar a los nuevos",
-            sourceUrl: "https://myapiadonix.vercel.app",
-            thumbnailUrl: "https://telegra.ph/file/24fa902ead26340f3df2c.png",
-            mediaType: 1,
-            renderLargerThumbnail: true
-          }
-        }
-      })
-    }
-
   } else if (command === 'off') {
     chat[option] = false
     m.reply(`✿ *${option}* desactivado ❌`)
